cd09d668e800b439a1ee3328edab23de363da081
Macbook <lei.ctzhu@gmail.com>
2018-10-04 23:12:43 -0400

    Adding decorator to App constructor

--- service.py
+++ service.py
@@ -1,16 +1,43 @@
+from zmq.eventloop.zmqstream import ZMQStream
+import functools
+import json
+import logging
+import os
 import tornado
 import tornado.ioloop
 import tornado.web
-from zmq.eventloop.zmqstream import ZMQStream
-# from tornado import gen, ioloop
-import zmq
-# from zmq.eventloop.future import Context
-import os
 import uuid
-import logging
-import json
+import zmq
 
 __UPLOADS__ = "uploads/"
+__POUT__ = 'tcp://127.0.0.1:12345'
+__PIN__ = 'tcp://127.0.0.1:12346'
+
+
+def response_callback(msg):
+    return [b'Yes']
+
+
+def addzmq_device(pubipport, repipport, callback):
+    def inner_func(func):
+        @functools.wraps(func)
+        def res_func(*args, **kwargs):
+            app = func(*args, **kwargs)
+            ctx = zmq.Context.instance()
+            sOut = ctx.socket(zmq.PUB)
+            sOut.bind(pubipport)
+            sIn = ctx.socket(zmq.REP)
+            sIn.bind(repipport)
+            streamOut = ZMQStream(sOut)
+            streamIn = ZMQStream(sIn)
+            def callback_in(msg):
+                streamIn.send_multipart(response_callback(msg))
+            streamIn.on_recv(callback_in)
+            app.__setattr__('streamIn', streamIn)
+            app.__setattr__('streamOut', streamOut)
+            return app
+        return res_func
+    return inner_func
 
 
 class Userform(tornado.web.RequestHandler):
@@ -46,42 +73,27 @@ class PathHandler(tornado.web.RequestHandler):
         self.finish(b'Done')
 
 
+@addzmq_device(__POUT__, __PIN__, response_callback)
 def make_app():
     # Here if you put capture groups in the endpoints definition
     # You either use all named capture groups or all position capture groups
     # Not a mixture of both
-    app = tornado.web.Application(
-            [
-             (r"/", Userform),
-             (r"/main", MainHandler),
-             (r"/upload", Upload),
-             (r"/rest/(?P<name>.*)/get/(?P<key>.*)", PathHandler),
-             (r"/rest/(.*)/get2/(.*)", PathHandler)
-            ], debug=True)
-    ctx = zmq.Context.instance()
-    sOut = ctx.socket(zmq.PUB)
-    sOut.bind('tcp://127.0.0.1:12345')
-    sIn = ctx.socket(zmq.REP)
-    sIn.bind('tcp://127.0.0.1:12346')
-    streamOut = ZMQStream(sOut)
-    streamIn = ZMQStream(sIn)
-    logging.info('init stream instance %s' % id(streamIn))
-
-    def callback_in(msg):
-        streamIn.send_multipart([b'Yes'])
-    streamIn.on_recv(callback_in)
-    app.__setattr__('streamIn', streamIn)
-    app.__setattr__('streamOut', streamOut)
-    return app
+    return tornado.web.Application([
+                (r"/", Userform),
+                (r"/main", MainHandler),
+                (r"/upload", Upload),
+                (r"/rest/(?P<name>.*)/get/(?P<key>.*)", PathHandler),
+                (r"/rest/(.*)/get2/(.*)", PathHandler)
+               ], debug=True)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     try:
+        loop = tornado.ioloop.IOLoop.current()
         app = make_app()
         app.listen(8999)
-        loop = tornado.ioloop.IOLoop.current()
         loop.start()
     except KeyboardInterrupt:
         pass
